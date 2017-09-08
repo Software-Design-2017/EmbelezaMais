28ea410aabc75f434b7db55dc999ce3424b5686a
igor-gme <igorgabriel515@gmail.com>
2017-09-08 12:24:50 -0300

    #5 - Adding User, Email and Name.
    Signed-off-by: igor-gme <igorgabriel515@gmail.com>
    Signed-off-by: thiagonf <thiagonf10@gmail.com>

---
+++ EmbelezaMais/user/constants.py
@@ -0,0 +1,19 @@
+# Project name.
+PROJECT_NAME = 'EmbelezaMais'
+
+# EMAIL FIELDS.
+EMAIL_FIELD_LENGTH = 30
+
+# USERNAME FIELDS.
+USERNAME_MAX_LENGHT = 12
+
+# FIRST NAME FIELDS.
+NAME_FIELD_LENGTH = 30
+NAME = 'First name'
+
+# NAME VALIDATION MESSAGES
+NAME_SIZE = 'Your name exceeds 30 characteres'
+NAME_CHARACTERS = 'Your name can\'t have special characters'
+
+# EMAIL VALIDATION MESSAGES.
+EMAIL_FORMAT = 'Enter a valid email address'
--- EmbelezaMais/user/models.py
+++ EmbelezaMais/user/models.py
@@ -1,3 +1,49 @@
-from django.db import models
+# standard library
+import logging
 
-# Create your models here.
+# Django.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin,
+)
+from django.db.models import EmailField
+from django.core import validators
+
+# Local Django.
+from . import constants
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(constants.PROJECT_NAME)
+
+
+class Email(EmailField):
+    validator = validators.EmailValidator(message=constants.EMAIL_FORMAT)
+
+    default_validators = [validator]
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = constants.EMAIL_FIELD_LENGTH
+        kwargs['default'] = ''
+        kwargs['unique'] = True
+        super(EmailField, self).__init__(*args, **kwargs)
+
+
+class Name(models.CharField):
+    validator_max_length = validators.MaxLengthValidator(constants.
+                                                         USERNAME_MAX_LENGHT,
+                                                         message=constants.
+                                                         NAME_SIZE)
+    validator_format = validators.RegexValidator(regex=r'^[A-Za-z ]+$',
+                                                 message=constants.
+                                                 NAME_CHARACTERS)
+    default_validators = [validator_max_length, validator_format]
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = constants.NAME_FIELD_LENGTH
+        kwargs['default'] = ''
+        super(models.CharField, self).__init__(*args, **kwargs)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = Email()
+    name = Name()
