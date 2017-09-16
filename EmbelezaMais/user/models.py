8f3078e2e74178ffa3a3060819b9d66df5c27bc8
igor-gme <igorgabriel515@gmail.com>
2017-09-16 10:05:42 -0300

    #5 - Fixing User and Email class.
    Signed-off-by: igor-gme <igorgabriel515@gmail.com>
    Signed-off-by: thiagonf <thiagonf10@gmail.com>

--- EmbelezaMais/user/models.py
+++ EmbelezaMais/user/models.py
@@ -1,54 +1,84 @@
-# standard library
-import logging
-
 # Django.
 from django.db import models
-# from django.contrib.auth.models import (
-#     AbstractBaseUser, PermissionsMixin,
-# )
-# from django.db.models import EmailField
-# from django.core import validators
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db.models import EmailField
+from django.contrib.auth.models import PermissionsMixin
+from django.core import validators
+# from django.utils.translation import ugettext_lazy as _
 
-# Local Django.
-from . import constants
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(constants.PROJECT_NAME)
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, name, **kwargs):
 
-#
-# class Email(EmailField):
-#     validator = validators.EmailValidator(message=constants.EMAIL_FORMAT)
-#
-#     default_validators = [validator]
-#
-#     def __init__(self, *args, **kwargs):
-#         kwargs['max_length'] = constants.EMAIL_FIELD_LENGTH
-#         kwargs['default'] = ''
-#         kwargs['unique'] = True
-#         super(EmailField, self).__init__(*args, **kwargs)
-#
-#
-# class Name(models.CharField):
-#     validator_max_length = validators.MaxLengthValidator(constants.
-#                                                          USERNAME_MAX_LENGHT,
-#                                                          message=constants.
-#                                                          NAME_SIZE)
-#     validator_format = validators.RegexValidator(regex=r'^[A-Za-z ]+$',
-#                                                  message=constants.
-#                                                  NAME_CHARACTERS)
-#     default_validators = [validator_max_length, validator_format]
-#
-#     def __init__(self, *args, **kwargs):
-#         kwargs['max_length'] = constants.NAME_FIELD_LENGTH
-#         kwargs['default'] = ''
-#         kwargs['unique'] = True
-#         super(models.CharField, self).__init__(*args, **kwargs)
-#
-#
-# class User(AbstractBaseUser, PermissionsMixin):
-#     USERNAME_FIELD = 'email'
-#     email = Email()
-#     name = Name()
+        if not email:
+            raise ValueError('The given email must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email,
+                          name=name,
+                          is_active=True,
+                          **kwargs)
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, email, password, name, **kwargs):
+
+        email = self.normalize_email(email)
+        user = self.model(email=email,
+                          name=name,
+                          is_staff=True,
+                          is_active=True,
+                          is_superuser=True,
+                          **kwargs)
+
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+
+class Email(EmailField):
+    validator = validators.EmailValidator(message='Enter a valid email address')
+
+    default_validators = [validator]
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 30
+        kwargs['default'] = ''
+        kwargs['unique'] = True
+        super(EmailField, self).__init__(*args, **kwargs)
+
+
+class Name(models.CharField):
+    validator_max_length = validators.MaxLengthValidator(60,
+                                                         message='Your name exceeds 30 characteres')
+    validator_format = validators.RegexValidator(regex=r'^[A-Za-z ]+$',
+                                                 message='Your name can\'t have special characters')
+    default_validators = [validator_max_length, validator_format]
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 60
+        kwargs['default'] = ''
+        super(models.CharField, self).__init__(*args, **kwargs)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    email = Email()
+    name = Name()
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    objects = UserManager()
+
+    def get_email(self):
+        return self.email
+
+    def get_name(self):
+        return self.name
 
 
 class TargetGenre(models.Model):
