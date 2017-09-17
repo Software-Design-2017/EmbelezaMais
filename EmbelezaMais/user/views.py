7cf72ca5b54f98cbfb12025dd15acbc7ce8cea8a
Marcelo Augusto <mekmay@hotmail.com>
2017-09-17 18:38:58 -0300

    #1 - Modularizing send email.
    Signed-off-by: Marcelo Augusto <mekmay@hotmail.com>
    Signed-off-by: Hugo Alves <hugo.asb96@gmail.com>

--- EmbelezaMais/user/views.py
+++ EmbelezaMais/user/views.py
@@ -38,34 +38,37 @@ def register_client_view(request):
         Client.objects.create_user(email=email, password=password,
                                    name=name, phone_number=phone_number)
 
-        # Prepare the information needed to send the account verification
-        # email.
-        salt = hashlib.sha1(str(random.random()).
-                            encode('utf-8')).hexdigest()[:5]
-        activation_key = hashlib.sha1(str(salt+email).
-                                      encode('utf‌​-8')).hexdigest()
-        key_expires = datetime.datetime.today() + datetime.timedelta(2)
-
         user = Client.objects.get(email=email)
-
-        new_profile = UserProfile(user=user, activation_key=activation_key,
-                                  key_expires=key_expires)
-        new_profile.save()
-
-        # Send account confirmation email.
-        email_subject = (constants.EMAIL_CONFIRMATION_SUBJECT)
-        email_body = constants.EMAIL_CONFIRMATION_BODY % (activation_key)
-
-        send_mail(email_subject, email_body, constants.EMBELEZAMAIS_EMAIL, [email],
-                  fail_silently=False)
-
-        logger.info("Email sended to user.")
-        return redirect('/')
-
+        return send_email_confirmation(user)
     else:
         logger.debug("Register form was invalid.")
 
     return render(request, "client_register_form.html", {"form": form})
+
+
+def send_email_confirmation(user):
+	# Prepare the information needed to send the account verification
+    # email.
+    salt = hashlib.sha1(str(random.random()).
+                        encode('utf-8')).hexdigest()[:5]
+    email = user.email
+    activation_key = hashlib.sha1(str(salt+email).
+                                  encode('utf‌​-8')).hexdigest()
+    key_expires = datetime.datetime.today() + datetime.timedelta(2)
+
+    new_profile = UserProfile(user=user, activation_key=activation_key,
+                              key_expires=key_expires)
+    new_profile.save()
+
+    # Send account confirmation email.
+    email_subject = (constants.EMAIL_CONFIRMATION_SUBJECT)
+    email_body = constants.EMAIL_CONFIRMATION_BODY % (activation_key)
+
+    send_mail(email_subject, email_body, constants.EMBELEZAMAIS_EMAIL, [email],
+              fail_silently=False)
+
+    logger.info("Email sended to user.")
+    return redirect('/')
 
 
 def register_confirm(request, activation_key):
