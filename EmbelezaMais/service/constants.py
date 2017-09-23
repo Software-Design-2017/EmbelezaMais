23140df417da18d88ddb5a183f455d3eff94b213
Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
2017-09-23 19:47:08 -0300

    #17 - Validating data.

---
+++ EmbelezaMais/service/constants.py
@@ -0,0 +1,13 @@
+# PART OF NAIL
+PART_CHOICES = ((1, 'Mão'), (2, 'Pé'), (3, 'Mão e Pé'))
+
+# SPECIFIC HAIR
+SPECIFIC_HAIR_CHOICES = ((1, 'Liso'), (2, 'Ondulado'), (3, 'Cacheado'), (4, 'Crespo'), (5, 'Geral'))
+
+# FACE PART
+FACE_PART_CHOICES = ((1, 'Rosto'), (2, 'Cílios'), (3, 'Sobrancelha'), (4, 'Geral'))
+
+MAKE_UP_SERVICE = 'Maquiagem'
+NAIL_SERVICE = "Manicure e Pedicure"
+BEARD_SERVICE = "Barba"
+HAIR_SERVICE = "Cabelo"
