c7019f0af90f0a4b9fc70d6b63de7146e6b3e36c
Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
2017-09-23 19:49:55 -0300

    #17 - Adding form for register services.

---
+++ EmbelezaMais/service/forms.py
@@ -0,0 +1,65 @@
+from django import forms
+
+from .models import (
+    Service, Combo, ServicesCombo, ServiceNail, ServiceMakeUp, ServiceBeard, ServiceHair
+)
+
+
+class ServiceForm(forms.ModelForm):
+    company_create = None
+
+    def __init__(self, **kw):
+        super(ServiceForm, self).__init__(**kw)
+        self.fields['service_in_combo'].queryset = Service.objects.filter(company=self.company_create,
+                                                                          combo__isnull=True)
+
+    @staticmethod
+    def __create_service_form__(company):
+        ServiceForm.company_create = company
+        return ServiceForm
+
+    class Meta:
+        model = ServicesCombo
+        fields = ['service_in_combo']
+
+
+class ComboForm():
+    company = None
+    ServiceComboFormSet = forms.inlineformset_factory(Combo, ServicesCombo,
+                                                      form=ServiceForm, extra=0,
+                                                      fk_name='service_combo',
+                                                      min_num=2, validate_min=True)
+
+    def __init__(self, company):
+        self.ServiceComboFormSet.form = ServiceForm.__create_service_form__(company=company)
+
+
+class FormService(forms.ModelForm):
+    class Meta:
+        model = Service
+        exclude = ['company']
+
+
+class ServiceNailForm(FormService):
+
+    class Meta:
+        model = ServiceNail
+        exclude = ['company']
+
+
+class ServiceMakeUpForm(FormService):
+    class Meta:
+        model = ServiceMakeUp
+        exclude = ['company']
+
+
+class ServiceBeardForm(FormService):
+    class Meta:
+        model = ServiceBeard
+        exclude = ['company']
+
+
+class ServiceHairForm(FormService):
+    class Meta:
+        model = ServiceHair
+        exclude = ['company']
