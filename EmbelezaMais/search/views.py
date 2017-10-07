fe0e8cd722d1b7561a9b886273df0729f50e4395
Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
2017-10-07 16:16:44 -0300

    #41 - Modifying template services.

--- EmbelezaMais/search/views.py
+++ EmbelezaMais/search/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.http import HttpResponseRedirect
 
 from user.models import Company
 
@@ -31,5 +30,29 @@ class CompanyDetail(DetailView):
     def get_context_data(self, **kwargs):
         context = super(CompanyDetail, self).get_context_data(**kwargs)
         context['company'] = Company.objects.get(pk=self.kwargs.get('pk'))
-        context['services'] = Service.objects.filter(company=context['company'])
+        services = Service.objects.filter(company=context['company'])
+        self.get_services(context, services)
+
         return context
+
+    def get_services(self, context, services):
+        context['services_nail'] = []
+        context['services_combo'] = []
+        context['services_makeup'] = []
+        context['services_hair'] = []
+        context['services_beard'] = []
+
+        for service in services:
+            if hasattr(service, 'servicenail'):
+                context['services_nail'].append(service)
+            elif hasattr(service, 'combo'):
+                context['services_combo'].append(service)
+            elif hasattr(service, 'servicemakeup'):
+                context['services_makeup'].append(service)
+            elif hasattr(service, 'servicehair'):
+                context['services_hair'].append(service)
+            elif hasattr(service, 'servicebeard'):
+                context['services_beard'].append(service)
+            else:
+                # Nothing to do.
+                pass
