f39321a929d2f3d1ecb10de3221445f265839aa6
Thiago Nogueira <thiagonf10@gmail.com>
2017-10-08 23:34:19 -0300

    #19 - Adding edit service class.

--- EmbelezaMais/service/views.py
+++ EmbelezaMais/service/views.py
@@ -1,12 +1,12 @@
 from django.core.urlresolvers import reverse_lazy
 from django.db import transaction
-from django.views.generic import CreateView, ListView, FormView
+from django.views.generic import CreateView, ListView, FormView, UpdateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.http import HttpResponseRedirect
 
 from .models import (
-    Service, Combo
+    Service, Combo, ServiceHair, ServiceNail, ServiceMakeUp, ServiceBeard
 )
 
 from .forms import (
@@ -202,3 +202,82 @@ class ServiceHairCreate(FormView):
         context = super(ServiceHairCreate, self).get_context_data(**kwargs)
         context['name'] = constants.HAIR_SERVICE
         return context
+
+
+class ServiceComboEdit(UpdateView):
+    model = Combo
+    fields = ['name', 'specification', 'description', 'price']
+    template_name = 'combo_service_form.html'
+    success_url = reverse_lazy('service_list')
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        services = context['services']
+
+        with transaction.atomic():
+            if services.is_valid():
+                services_validate = self._validate_quantity(services, form)
+            else:
+                return super(ServiceComboCreate, self).form_invalid(form)
+
+            # Register combo and your creator
+            self.object = form.save(commit=False)
+            self.object.company = self.request.user.company
+            self.object.save()
+
+            for service_in_combo in services_validate:
+                    self.object.combo.add(service_in_combo)
+
+            self.object.combo.save()
+            print(self.object.services)
+        return super(ServiceComboCreate, self).form_valid(form)
+
+    def _validate_quantity(self, services, form):
+        if len(services) > 1:
+            services_validate = []
+            number_of_filled = len(services)
+
+            # Checks the number of forms filled
+            for service_compound in services:
+                get_service = service_compound.cleaned_data.get('service_in_combo')
+
+                # Case form is filled the object is add in list of combo
+                if get_service:
+                    services_validate.append(service_compound.cleaned_data.get('service_in_combo'))
+                else:
+                    number_of_filled = number_of_filled - 1
+
+            if number_of_filled > 1:
+                return services_validate
+            else:
+                return super(ServiceComboCreate, self).form_invalid(form)
+        else:
+            return super(ServiceComboCreate, self).form_invalid(form)
+
+
+class ServiceNailEdit(UpdateView):
+    model = ServiceNail
+    template_name = 'service_form.html'
+    form_class = ServiceNailForm
+    success_url = '/service/list/'
+
+
+class ServiceMakeUpEdit(UpdateView):
+    model = ServiceMakeUp
+    template_name = 'service_form.html'
+    form_class = ServiceMakeUpForm
+    success_url = '/service/list/'
+
+
+class ServiceBeardEdit(UpdateView):
+    model = ServiceBeard
+    template_name = 'service_form.html'
+    form_class = ServiceBeardForm
+    success_url = '/service/list/'
+
+
+class ServiceHairEdit(UpdateView):
+    model = ServiceHair
+    template_name = 'service_form.html'
+    form_class = ServiceHairForm
+    success_url = '/service/list/'
