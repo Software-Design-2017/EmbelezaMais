366001c5394196dd4eaa33574e83e0b61e21ab96
Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
2017-10-11 11:35:14 -0300

    #25 - Creating geolocation js.

---
+++ EmbelezaMais/static/company/map_canvas.js
@@ -0,0 +1,75 @@
+var position = [40.748774, -73.985763];
+
+      function initialize() {
+      var latlng = new google.maps.LatLng(position[0], position[1]);
+      var myOptions = {
+          zoom: 16,
+          center: latlng,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+
+      var input = document.getElementById('pac-input');
+      var searchBox = new google.maps.places.SearchBox(input);
+      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+      marker = new google.maps.Marker({
+          position: latlng,
+          map: map,
+          title: "Latitude:"+position[0]+" | Longitude:"+position[1]
+      });
+
+      google.maps.event.addListener(map, 'click', function(event) {
+          var result = [event.latLng.lat(), event.latLng.lng()];
+          transition(result);
+          document.getElementById("id_latitude") = position[0];
+          document.getElementById("id_longitude") = position[1];
+      });
+
+      google.maps.event.addListener(searchBox, 'places_changed', function() {
+          var places = searchBox.getPlaces();
+          var bounds = new google.maps.LatLngBounds();
+          var i, place;
+
+          for(i=0; place=places[i]; i++){
+            bounds.extend(place.geometry.location);
+            marker.setPosition(place.geometry.location);
+            position[0]= place.geometry.location.lat();
+            position[1]= place.geometry.location.lng();
+          }
+          map.fitBounds(bounds);
+          map.setZoom(15);
+          document.getElementById("id_latitude").value = position[0];
+          document.getElementById("id_longitude").value = position[1];
+      });
+      }
+
+      //Load google map
+      google.maps.event.addDomListener(window, 'load', initialize);
+
+
+      var numDeltas = 100;
+      var delay = 10; //milliseconds
+      var i = 0;
+      var deltaLat;
+      var deltaLng;
+
+      function transition(result){
+        i = 0;
+        deltaLat = (result[0] - position[0])/numDeltas;
+        deltaLng = (result[1] - position[1])/numDeltas;
+        moveMarker();
+      }
+
+      function moveMarker(){
+        position[0] += deltaLat;
+        position[1] += deltaLng;
+        var latlng = new google.maps.LatLng(position[0], position[1]);
+        marker.setTitle("Latitude:"+position[0]+" | Longitude:"+position[1]);
+        marker.setPosition(latlng);
+        if(i!=numDeltas){
+            i++;
+            setTimeout(moveMarker, delay);
+        }
+      }
