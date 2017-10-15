b0207efa6535698b2a82a3fb57c84383c6fedc3b
Igor Gabriel <igorgabriel515@gmail.com>
2017-10-15 21:13:04 -0200

    Fixing Register class.

--- EmbelezaMais/user/views.py
+++ EmbelezaMais/user/views.py
@@ -40,111 +40,113 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('EmbelezaMais')
 
 
-def register_client_view(request):
-    form = ClientRegisterForm(request.POST or None)
+class ClientRegisterView(View):
+    def register_client(request):
+        form = ClientRegisterForm(request.POST or None)
 
-    if form.is_valid():
-        logger.debug("Register form was valid.")
-        email = form.cleaned_data.get('email')
-        password = form.cleaned_data.get('password')
-        name = form.cleaned_data.get('name')
-        phone_number = form.cleaned_data.get('phone_number')
-        Client.objects.create_user(email=email, password=password,
-                                   name=name, phone_number=phone_number)
+        if form.is_valid():
+            logger.debug("Register form was valid.")
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            name = form.cleaned_data.get('name')
+            phone_number = form.cleaned_data.get('phone_number')
+            Client.objects.create_user(email=email, password=password,
+                                       name=name, phone_number=phone_number)
 
-        user = Client.objects.get(email=email)
-        return send_email_confirmation(user)
-    else:
-        logger.debug("Register form was invalid.")
+            user = Client.objects.get(email=email)
+            return CountConfirmation.send_email_confirmation(user)
+        else:
+            logger.debug("Register form was invalid.")
 
-    return render(request, "client_register_form.html", {"form": form})
-
-
-def register_company_view(request):
-    form = CompanyRegisterForm(request.POST or None)
-    if form.is_valid():
-        email = form.cleaned_data.get('email')
-        password = form.cleaned_data.get('password')
-        name = form.cleaned_data.get('name')
-        description = form.cleaned_data.get('description')
-        target_genre = form.cleaned_data.get('target_genre')
-        have_parking_availability = form.cleaned_data.get('have_parking_availability')
-        latitude = float(str(form.cleaned_data.get('latitude')))
-        longitude = float(str(form.cleaned_data.get('longitude')))
-        position = Geoposition(latitude, longitude)
-        Company.objects.create_user(email=email, password=password, name=name,
-                                    target_genre=target_genre, have_parking_availability=have_parking_availability,
-                                    description=description, position=position)
-
-        user = Company.objects.get(email=email)
-
-        return send_email_confirmation(user)
-
-    else:
-        pass
-
-    return render(request, "company_register_form.html", {"form": form})
+        return render(request, "client_register_form.html", {"form": form})
 
 
-def send_email_confirmation(user):
-    # Prepare the information needed to send the account verification
-    # email.
-    salt = hashlib.sha1(str(random.random()).
-                        encode('utf-8')).hexdigest()[:5]
-    email = user.email
+class CompanyResgisterView(View):
+    def register_company(request):
+        form = CompanyRegisterForm(request.POST or None)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            name = form.cleaned_data.get('name')
+            description = form.cleaned_data.get('description')
+            target_genre = form.cleaned_data.get('target_genre')
+            have_parking_availability = form.cleaned_data.get('have_parking_availability')
+            latitude = float(str(form.cleaned_data.get('latitude')))
+            longitude = float(str(form.cleaned_data.get('longitude')))
+            position = Geoposition(latitude, longitude)
+            Company.objects.create_user(email=email, password=password, name=name,
+                                        target_genre=target_genre, have_parking_availability=have_parking_availability,
+                                        description=description, position=position)
 
-    activation_key = hashlib.sha1(str(salt+email).
-                                  encode('utf‌​-8')).hexdigest()
-    key_expires = datetime.datetime.today() + datetime.timedelta(2)
+            user = Company.objects.get(email=email)
 
-    new_profile = UserProfile(user=user, activation_key=activation_key,
-                              key_expires=key_expires)
-    new_profile.save()
+            return CountConfirmation.send_email_confirmation(user)
 
-    # Send account confirmation email.
-    email_subject = (constants.EMAIL_CONFIRMATION_SUBJECT)
-    email_body = constants.EMAIL_CONFIRMATION_BODY % (activation_key)
+        else:
+            pass
 
-    send_mail(email_subject, email_body, constants.EMBELEZAMAIS_EMAIL, [email],
-              fail_silently=False)
-
-    logger.info("Email sended to user.")
-    return redirect('/')
+        return render(request, "company_register_form.html", {"form": form})
 
 
-def register_confirm(request, activation_key):
-    # Check if activation token is valid, if not valid return an 404 error.
-    # Verify if user is already confirmed.
-    if request.user.id is not None:
+class CountConfirmation(View):
+    def send_email_confirmation(user):
+        # Prepare the information needed to send the account verification
+        # email.
+        salt = hashlib.sha1(str(random.random()).
+                            encode('utf-8')).hexdigest()[:5]
+        email = user.email
 
-        logger.info("Logged user: " + request.user.name)
+        activation_key = hashlib.sha1(str(salt+email).
+                                      encode('utf‌​-8')).hexdigest()
+        key_expires = datetime.datetime.today() + datetime.timedelta(2)
 
-        HttpResponse('Conta ja confirmada')
-    else:
-        # Nothing to do
-        pass
+        new_profile = UserProfile(user=user, activation_key=activation_key,
+                                  key_expires=key_expires)
+        new_profile.save()
 
-    user_profile = get_object_or_404(UserProfile,
-                                     activation_key=activation_key)
+        # Send account confirmation email.
+        email_subject = (constants.EMAIL_CONFIRMATION_SUBJECT)
+        email_body = constants.EMAIL_CONFIRMATION_BODY % (activation_key)
 
-    # Verifies if the activation token has expired and if so renders the html
-    # of expired registration.
-    if user_profile.key_expires < timezone.now():
-        user_profile.delete()
+        send_mail(email_subject, email_body, constants.EMBELEZAMAIS_EMAIL, [email],
+                  fail_silently=False)
 
-        return HttpResponse("Tempo para confirmar conta expirou." +
-                            "Crie sua conta novamente")
-    else:
-        # Nothing to do.
-        pass
+        logger.info("Email sended to user.")
+        return redirect('/')
 
-    # If the token has not expired, the user is activated and the
-    # confirmation html is displayed.
-    user = user_profile.user
-    user.is_active = True
-    user.save()
+    def register_confirm(request, activation_key):
+        # Check if activation token is valid, if not valid return an 404 error.
+        # Verify if user is already confirmed.
+        if request.user.id is not None:
 
-    return redirect('/')
+            logger.info("Logged user: " + request.user.name)
+
+            HttpResponse('Conta ja confirmada')
+        else:
+            # Nothing to do
+            pass
+
+        user_profile = get_object_or_404(UserProfile,
+                                         activation_key=activation_key)
+
+        # Verifies if the activation token has expired and if so renders the html
+        # of expired registration.
+        if user_profile.key_expires < timezone.now():
+            user_profile.delete()
+
+            return HttpResponse("Tempo para confirmar conta expirou." +
+                                "Crie sua conta novamente")
+        else:
+            # Nothing to do.
+            pass
+
+        # If the token has not expired, the user is activated and the
+        # confirmation html is displayed.
+        user = user_profile.user
+        user.is_active = True
+        user.save()
+
+        return redirect('/')
 
 
 class ClientProfile(View):
